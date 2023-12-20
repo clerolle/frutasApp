b943a914bdccbad0f2329c16cd937938ec915435
unknown <clerollep@ucentral.edu.co>
2023-12-20 16:53:45 -0500

    ajuste responsive de la tarjeta

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -13,10 +13,10 @@ import Paper from '@mui/material/Paper';
 import { fruitsAvailable } from '../../../data';
 
 const Item = styled('div')(({ theme }) => ({
-  backgroundColor: theme.palette.mode ===  '#1A2027',
+  backgroundColor: theme.palette.mode === '#1A2027',
   border: '1px solid',
   borderColor: theme.palette.mode === 'dark' ? '#444d58' : '#444d58',
-  boxShadow: "10px 10px 30px black", 
+  boxShadow: "10px 10px 30px black",
   padding: theme.spacing(1),
   borderRadius: '4px',
   textAlign: 'center',
@@ -24,23 +24,26 @@ const Item = styled('div')(({ theme }) => ({
 
 function PageProdcuts(props) {
 
-    const [ fruits, setFruits] = useState(fruitsAvailable);
-    
-    return (
-        <div /* style={{background: "black"}} */>
-            <ResponsiveAppBar />
-            <br />
-            <Grid container >
-                <Grid item display={"flex"} flexWrap={'wrap'} justifyContent={"center"} gap={5}>
-                {fruits.map(fruit => 
-                    <Item key={fruit.name}>
-                      <ReviewCard fruit={fruit} key={fruit.name}/>
-                    </Item>
-                )} 
-                </Grid>
-            </Grid>   
-        </div>
-    );
+  const [fruits, setFruits] = useState(fruitsAvailable);
+
+  return (
+    <div /* style={{background: "black"}} */>
+      <ResponsiveAppBar />
+      <br />
+      <Grid container >
+        <Grid item 
+          display={"flex"} flexWrap={'wrap'} justifyContent={"center"} 
+          gap={5} md={12} xs={10} marginLeft={4}
+        >
+          {fruits.map(fruit =>
+            <Item key={fruit.name}>
+              <ReviewCard fruit={fruit} key={fruit.name} />
+            </Item>
+          )}
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default PageProdcuts;
